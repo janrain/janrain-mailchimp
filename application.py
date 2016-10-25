73550c6974acf9c16dbd0a714ba79621c88f7be6
Cameron White <cameron.white@janrain.com>
2016-10-25 12:42:03 -0700

    DIP-471 Fixed application.py to define application correctly

--- application.py
+++ application.py
@@ -3,12 +3,11 @@ from janrain_mailchimp_connect.models import JobModel
 from janrain_mailchimp_connect import create_app, logging_init
 from janrain_mailchimp_connect.config import get_config
 
-
+config = get_config()
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+# must be named application for beanstalk to find it automatically
+application = create_app(config, JobModel, executor)
+logging_init(application)
 
 if __name__ == '__main__':
-    config = get_config()
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-    # must be named application for beanstalk to find it automatically
-    application = create_app(config, JobModel, executor)
-    logging_init(application)
     application.run()
